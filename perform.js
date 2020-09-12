050b45ab937d613c92a35bd560f270dfa1328f9c
NodeBE4 <becare4@tutanota.com>
2020-09-12 18:29:15 +0000

    init

--- perform.js
+++ perform.js
@@ -5,21 +5,10 @@ let urlMod = require('url')
 let URL = urlMod.URL
 
 let feedxUrls = {
-  '火光': 'https://2049post.wordpress.com/feed/',
-  '萬有引力之蟲': 'http://gravitysworm.tumblr.com/rss',
-  '编程随想的博客': 'https://feeds2.feedburner.com/programthink?format=xml',
-  '透視中國-YouTube': 'http://www.youtube.com/feeds/videos.xml?playlist_id=UUGekZ_Ig4dP3NDcJCdOK6aA',
-  '野兽爱智慧': 'https://rsshub.app/matters/author/philosophia1979',
-  'The Sociologist': 'https://sociologist.xyz/feed.xml',
-  'The Sociologist TG': 'https://rsshub.app/telegram/channel/thesoc',
-  '衔枚疾进': 'https://rsshub.app/telegram/channel/silentmarching',
-  '政見': 'http://cnpolitics.org/feed/',
-  '中國戰略分析': 'http://zhanlve.org/?feed=rss2',
-  'Stratechery': 'https://stratechery.com/feed/',
-  'NGOCN': 'https://rsshub.app/matters/author/ngocncat',
-  '蔷蔷': 'http://www.youtube.com/feeds/videos.xml?playlist_id=UC_Udz5R0NCgLTWbmn-QiWGA',
-  '李肃Hi5': 'https://www.youtube.com/feeds/videos.xml?playlist_id=PLmp6SED1O-uyPp8NoKmnHEsm3-BwOSyg1',
-  '臺大演講網': 'http://www.youtube.com/feeds/videos.xml?playlist_id=UUSgvLn9EzRHS7yOJqXcJ68Q',
+  '不吐不快的有货君': 'http://www.youtube.com/feeds/videos.xml?playlist_id=UUL_6y6pug2cYoYCYvXDbgHA',
+  '想做教育家的Klaus': 'https://www.youtube.com/feeds/videos.xml?playlist_id=UUZ4NwvuGYgFyjnRRJekdnHw',
+  '書齋夜話': 'https://www.youtube.com/feeds/videos.xml?playlist_id=UUhRE0pMeij_O5FqWrSKBF-Q',
+  '温相说党史': 'https://www.youtube.com/feeds/videos.xml?playlist_id=UUGtvPQxpuRrXeryEWxQkAkA',
 }
 
 async function fetchArticles(site) {
